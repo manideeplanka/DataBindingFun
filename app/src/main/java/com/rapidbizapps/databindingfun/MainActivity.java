7c583e26404059ec615ecabfcac31afb822c6e01
manideeplanka <mlanka@rapidbizapps.com>
2016-11-09 16:55:37 +0530

    feat app: Data binding support added in gradle

--- app/src/main/java/com/rapidbizapps/databindingfun/MainActivity.java
+++ app/src/main/java/com/rapidbizapps/databindingfun/MainActivity.java
@@ -1,14 +1,23 @@
 package com.rapidbizapps.databindingfun;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.rapidbizapps.databindingfun.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mainBinding.greeting.setText("Glad to see you");
+        mainBinding.button1.setText("Click1");
+        mainBinding.button2.setText("Click2");
+
 
 
     }
